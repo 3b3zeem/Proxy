8bd89f87db2bf9c133cf56343df4760c4694ad9e
3b3zeem <ahmedzezo2247@gmail.com>
2025-08-17 00:47:59 +0300

    Edit Clients Images format

--- Components/Clients.tsx
+++ Components/Clients.tsx
@@ -7,7 +7,7 @@ const Clients = () => {
     >
       <h1 className="md:text-4xl text-3xl mb-10 font-[Lelevien]">Clients</h1>
       <div className="flex justify-end w-full">
-        <div className="flex flex-col md:items-center items-end gap-6">
+        <div className="flex flex-col items-center gap-6">
           <div className="flex gap-6">
             {Array(5).fill(null).map((_, index) => (
               <div key={index} className="md:w-16 md:h-16 w-10 h-10 border-2 border-white rounded-full"></div>
